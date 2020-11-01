b98a67b23096d615c44a90f4ef445275ac493f62
Simptom569 <pikalovegor0@gmail.com>
2020-11-01 19:39:00 +0300

    edit task template

--- task/views.py
+++ task/views.py
@@ -1,10 +1,20 @@
 from django.shortcuts import render
+from django.http import Http404
+from django.contrib.auth.models import User
 from .forms import CreateTaskForm
-from .models import Task
+from .models import Task, Users
 
 
 def tasks(request):
-	return render(request, 'task/tasks.html')
+	user = request.user.id
+	try:
+		user = Users.objects.get(user_id=user)
+	except:
+		user = Users(user_id=user, user_password='12345')
+		user.save()
+	user = user.user_task.all()
+	context = {'task': user}
+	return render(request, 'task/tasks.html', context)
 
 
 def create(request):
@@ -18,11 +28,14 @@ def create(request):
 
 
 def task(request, pk):
-	task = Task.objects.get(task_id=pk)
-	task_id = task.task_id
-	task_password = task.task_password
-	context = {
-	'id': task_id,
-	'password': task_password,
-	}
+	try:
+		task = Task.objects.get(task_id=pk)
+		task_id = task.task_id
+		task_password = task.task_password
+		context = {
+		'id': task_id,
+		'password': task_password,
+		}
+	except Task.DoesNotExist:
+		raise Http404("Task does not exist")
 	return render(request, 'task/task.html', context)
